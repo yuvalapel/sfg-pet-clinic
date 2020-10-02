fd6fa821afe3ad6c064170c9121e734eaef89521
yuval <yuvalapel6@gmail.com>
2020-10-02 12:28:54 +0300

    added specialityService

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/services/SpecialtiesService.java
@@ -0,0 +1,8 @@
+package guru.springframework.sfgpetclinic.model.services;
+
+import guru.springframework.sfgpetclinic.model.Speciality;
+
+public interface SpecialtiesService extends CrudService<Speciality, Long> {
+
+
+}
