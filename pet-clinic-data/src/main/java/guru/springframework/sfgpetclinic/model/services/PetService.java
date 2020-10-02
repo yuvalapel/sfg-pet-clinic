b21817cc6711db5bd5e0045aba3710b7e57cb4a5
yuval <yuvalapel6@gmail.com>
2020-10-02 11:41:28 +0300

    added PetTypeService

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/services/PetService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/services/PetService.java
@@ -1,9 +1,7 @@
 package guru.springframework.sfgpetclinic.model.services;
 
-import guru.springframework.sfgpetclinic.model.Owner;
 import guru.springframework.sfgpetclinic.model.Pet;
 
-import java.util.Set;
 
 public interface PetService extends CrudService<Pet, Long> {
 }
---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/services/PetTypeService.java
@@ -0,0 +1,8 @@
+package guru.springframework.sfgpetclinic.model.services;
+
+import guru.springframework.sfgpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+
+
+}
